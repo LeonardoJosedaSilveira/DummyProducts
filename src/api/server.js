8709f61ac07d2cb9ab0c2836e1733db35f88b6e2
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-11-27 22:42:38 -0300

    start project root folders

---
+++ src/api/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const app = require('./app');
+
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  if (err.status) return res.status(err.status).json({ message: err.message});
+  return res.status(500).json({ message: err.message});
+});
+
+app.listen(port)
+
+console.log(`Server start in port ${port}`);
